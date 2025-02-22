2eb0dbf26f3d21820b8b358f73dcdab20cff012c
Maksym Druziuk <druziukmaksym@gmail.com>
2025-02-22 13:53:48 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= 'Study';?></title>
+</head>
+<body>
+    <?php
+        echo 'Hello World!';
+    ?>
+    <?php
+        echo 5+3+2;
+    ?>
+</body>
+</html>
